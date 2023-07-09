fb35080273e155e6a430b19772883c91d4fa7f87
chandansah0 <chandanprasadsah9@gmail.com>
2023-07-09 10:26:34 +0545

    MERN web development

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
+//import Home from './components/Home';
+
+function App() {
+  return <Login/>;
+  //return <Home/>;
+}
+
+export default App;
